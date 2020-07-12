0cfd50f61b29f5f417962f26596bf01b0c6ba1a8
liuyuanyuan <1445179059@qq.com>
2020-07-12 23:26:12 +0800

    echart 学习完毕

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // 监控区域模块制作
-(function() {
-    $(".monitor .tabs").on("click", "a", function() {
+(function () {
+    $(".monitor .tabs").on("click", "a", function () {
         $(this)
             .addClass("active")
             .siblings("a")
@@ -15,7 +15,7 @@
             .hide();
     });
     // 1. 先克隆marquee里面所有的行（row）
-    $(".marquee-view .marquee").each(function() {
+    $(".marquee-view .marquee").each(function () {
         // console.log($(this));
         var rows = $(this)
             .children()
@@ -155,6 +155,7 @@
                     alignWithLabel: false, //刻度在柱子中间显示，不好看
                     show: false,// 把x轴的刻度隐藏起来
                 },
+                // 修饰刻度标签的颜色
                 axisLabel: {
                     color: "#4c9bfd"  //刻度标签颜色
                 },
@@ -225,7 +226,7 @@
 })();
 
 // 销售统计模块
-(function() {
+(function () {
     // (1)准备数据
     var data = {
         year: [
@@ -262,7 +263,6 @@
                 color: "#4c9bfd"
             }
             // 如果series 里面设置了name，此时图例组件的data可以省略
-            // data: ["邮件营销", "联盟广告"]
         },
         grid: {
             top: "20%",
@@ -273,10 +273,9 @@
             borderColor: "#012f4a",
             containLabel: true
         },
-
         xAxis: {
             type: "category",
-            boundaryGap: false,
+            boundaryGap: false,  //轴两端不需要内边距
             data: [
                 "1月",
                 "2月",
@@ -339,49 +338,38 @@
             }
         ]
     };
-    // 3. 把配置和数据给实例对象
     myChart.setOption(option);
-    // 4. tab切换效果制作
-    // (2) 点击切换效果
-    $(".sales .caption").on("click", "a", function() {
-        // 此时要注意这个索引号的问题
-        index = $(this).index() - 1;
-        // 点击当前a 高亮显示 调用active
+    // tab切换效果制作
+
+    $(".sales .caption").on("click", "a", function () {
+        index = $(this).index() - 1; // 此时要注意这个索引号的问题
         $(this)
             .addClass("active")
             .siblings("a")
             .removeClass("active");
-        // 拿到当前a 的自定义属性值
-        // console.log(this.dataset.type);
-        // 根据拿到的值 去找数据
-        // console.log(data.year);
-        // console.log(data["year"]);
-        // console.log(data[this.dataset.type]);
-        var arr = data[this.dataset.type];
-        // console.log(arr);
-        // 根据拿到的数据重新渲染 series里面的data值
+        var arr = data[this.dataset.type]; //// 根据拿到的值 去找数据
         option.series[0].data = arr[0];
         option.series[1].data = arr[1];
-        // 重新把配置好的新数据给实例对象
         myChart.setOption(option);
-    });
-    // 5. tab栏自动切换效果
-    //  开启定时器每隔3s，自动让a触发点击事件即可
+    });  //注册了一个点击事件
+
+    //  tab栏自动切换效果  开启定时器每隔3s，自动让a触发点击事件即可
     var as = $(".sales .caption a");
     var index = 0;
-    var timer = setInterval(function() {
+    var timer = setInterval(function () {
         index++;
         if (index >= 4) index = 0;
         as.eq(index).click();
     }, 1000);
     // 鼠标经过sales，关闭定时器，离开开启定时器
+    //！！！$( selector ).hover( handlerIn, handlerOut )===$( selector ).mouseenter( handlerIn ).mouseleave( handlerOut );
     $(".sales").hover(
-        function() {
+        function () {
             clearInterval(timer);
         },
-        function() {
+        function () {
             clearInterval(timer);
-            timer = setInterval(function() {
+            timer = setInterval(function () {
                 index++;
                 if (index >= 4) index = 0;
                 as.eq(index).click();
@@ -389,57 +377,50 @@
         }
     );
     // 当我们浏览器缩放的时候，图表也等比例缩放
-    window.addEventListener("resize", function() {
-        // 让我们的图表调用 resize这个方法
+    window.addEventListener("resize", function () {
         myChart.resize();
     });
 })();
-// 销售渠道模块 雷达图
-(function() {
-    // 1. 实例化对象
-    var myChart = echarts.init(document.querySelector(".radar"));
-    // 2.指定配置
 
+
+//雷达图
+(function () {
+    var myChart = echarts.init(document.querySelector(".radar"));
     var option = {
         tooltip: {
             show: true,
-            // 控制提示框组件的显示位置
-            position: ["60%", "10%"]
+            position: ["60%", "10%"]  // 控制提示框组件的显示位置
         },
         radar: {
             indicator: [
-                { name: "机场", max: 100 },
-                { name: "商场", max: 100 },
-                { name: "火车站", max: 100 },
-                { name: "汽车站", max: 100 },
-                { name: "地铁", max: 100 }
+                {name: "机场", max: 100},
+                {name: "商场", max: 100},
+                {name: "火车站", max: 100},
+                {name: "汽车站", max: 100},
+                {name: "地铁", max: 100}
             ],
-            // 修改雷达图的大小
-            radius: "65%",
+            radius: "65%",   // 修改雷达图的大小
             shape: "circle",
-            // 分割的圆圈个数
-            splitNumber: 4,
+            splitNumber: 4,// 分割的圆圈个数
+            splitLine: {
+                lineStyle: {
+                    color: "rgba(255,255,255, 0.5)"
+                }
+            }, // 分割的圆圈线条的样式
             name: {
                 // 修饰雷达图文字的颜色
                 textStyle: {
                     color: "#4c9bfd"
                 }
             },
-            // 分割的圆圈线条的样式
-            splitLine: {
-                lineStyle: {
-                    color: "rgba(255,255,255, 0.5)"
-                }
-            },
             splitArea: {
                 show: false
             },
-            // 坐标轴的线修改为白色半透明
             axisLine: {
                 lineStyle: {
                     color: "rgba(255, 255, 255, 0.5)"
                 }
-            }
+            } // 坐标轴的线修改为白色半透明
         },
         series: [
             {
@@ -474,38 +455,30 @@
             }
         ]
     };
-    // 3.把配置和数据给对象
     myChart.setOption(option);
     // 当我们浏览器缩放的时候，图表也等比例缩放
-    window.addEventListener("resize", function() {
-        // 让我们的图表调用 resize这个方法
+    window.addEventListener("resize", function () {
         myChart.resize();
     });
 })();
-// 销售模块 饼形图 半圆形 设置方式
-(function() {
-    // 1. 实例化对象
+
+// 饼形图 半圆形
+(function () {
     var myChart = echarts.init(document.querySelector(".gauge"));
-    // 2. 指定数据和配置
     var option = {
         series: [
             {
                 name: "销售进度",
                 type: "pie",
                 radius: ["130%", "150%"],
-                // 移动下位置  套住50%文字
-                center: ["48%", "80%"],
-                //是否启用防止标签重叠策略
-                // avoidLabelOverlap: false,
+                center: ["48%", "80%"],   // 移动下位置  套住50%文字
                 labelLine: {
                     normal: {
                         show: false
                     }
                 },
-                // 饼形图的起始角度为 180， 注意不是旋转角度
-                startAngle: 180,
-                // 鼠标经过不需要放大偏移图形
-                hoverOffset: 0,
+                startAngle: 180, // 饼形图的起始角度为 180， 注意不是旋转角度
+                hoverOffset: 0,   // 鼠标经过不需要放大偏移图形
                 data: [
                     {
                         value: 100,
@@ -518,8 +491,8 @@
                                 0,
                                 1,
                                 [
-                                    { offset: 0, color: "#00c9e0" }, // 0 起始颜色
-                                    { offset: 1, color: "#005fc1" } // 1 结束颜色
+                                    {offset: 0, color: "#00c9e0"}, // 0 起始颜色
+                                    {offset: 1, color: "#005fc1"} // 1 结束颜色
                                 ]
                             )
                         }
@@ -540,17 +513,15 @@
             }
         ]
     };
-    // 3. 把数据和配置给实例对象
     myChart.setOption(option);
     // 当我们浏览器缩放的时候，图表也等比例缩放
-    window.addEventListener("resize", function() {
-        // 让我们的图表调用 resize这个方法
+    window.addEventListener("resize", function () {
         myChart.resize();
     });
 })();
+
 // 全国热榜模块
-(function() {
-    // 1. 准备相关数据
+(function () {
     var hotData = [
         {
             city: "北京", // 城市
@@ -558,12 +529,12 @@
             flag: true, //  上升还是下降
             brands: [
                 //  品牌种类数据
-                { name: "可爱多", num: "9,086", flag: true },
-                { name: "娃哈哈", num: "8,341", flag: true },
-                { name: "喜之郎", num: "7,407", flag: false },
-                { name: "八喜", num: "6,080", flag: false },
-                { name: "小洋人", num: "6,724", flag: false },
-                { name: "好多鱼", num: "2,170", flag: true }
+                {name: "可爱多", num: "9,086", flag: true},
+                {name: "娃哈哈", num: "8,341", flag: true},
+                {name: "喜之郎", num: "7,407", flag: false},
+                {name: "八喜", num: "6,080", flag: false},
+                {name: "小洋人", num: "6,724", flag: false},
+                {name: "好多鱼", num: "2,170", flag: true}
             ]
         },
         {
@@ -571,12 +542,12 @@
             sales: "23,252",
             flag: false,
             brands: [
-                { name: "可爱多", num: "3,457", flag: false },
-                { name: "娃哈哈", num: "2,124", flag: true },
-                { name: "喜之郎", num: "8,907", flag: false },
-                { name: "八喜", num: "6,080", flag: true },
-                { name: "小洋人", num: "1,724", flag: false },
-                { name: "好多鱼", num: "1,170", flag: false }
+                {name: "可爱多", num: "3,457", flag: false},
+                {name: "娃哈哈", num: "2,124", flag: true},
+                {name: "喜之郎", num: "8,907", flag: false},
+                {name: "八喜", num: "6,080", flag: true},
+                {name: "小洋人", num: "1,724", flag: false},
+                {name: "好多鱼", num: "1,170", flag: false}
             ]
         },
         {
@@ -584,12 +555,12 @@
             sales: "20,760",
             flag: true,
             brands: [
-                { name: "可爱多", num: "2,345", flag: true },
-                { name: "娃哈哈", num: "7,109", flag: true },
-                { name: "喜之郎", num: "3,701", flag: false },
-                { name: "八喜", num: "6,080", flag: false },
-                { name: "小洋人", num: "2,724", flag: false },
-                { name: "好多鱼", num: "2,998", flag: true }
+                {name: "可爱多", num: "2,345", flag: true},
+                {name: "娃哈哈", num: "7,109", flag: true},
+                {name: "喜之郎", num: "3,701", flag: false},
+                {name: "八喜", num: "6,080", flag: false},
+                {name: "小洋人", num: "2,724", flag: false},
+                {name: "好多鱼", num: "2,998", flag: true}
             ]
         },
         {
@@ -597,12 +568,12 @@
             sales: "23,252",
             flag: false,
             brands: [
-                { name: "可爱多", num: "2,156", flag: false },
-                { name: "娃哈哈", num: "2,456", flag: true },
-                { name: "喜之郎", num: "9,737", flag: true },
-                { name: "八喜", num: "2,080", flag: true },
-                { name: "小洋人", num: "8,724", flag: true },
-                { name: "好多鱼", num: "1,770", flag: false }
+                {name: "可爱多", num: "2,156", flag: false},
+                {name: "娃哈哈", num: "2,456", flag: true},
+                {name: "喜之郎", num: "9,737", flag: true},
+                {name: "八喜", num: "2,080", flag: true},
+                {name: "小洋人", num: "8,724", flag: true},
+                {name: "好多鱼", num: "1,770", flag: false}
             ]
         },
         {
@@ -610,81 +581,68 @@
             sales: "20,760",
             flag: true,
             brands: [
-                { name: "可爱多", num: "9,567", flag: true },
-                { name: "娃哈哈", num: "2,345", flag: false },
-                { name: "喜之郎", num: "9,037", flag: false },
-                { name: "八喜", num: "1,080", flag: true },
-                { name: "小洋人", num: "4,724", flag: false },
-                { name: "好多鱼", num: "9,999", flag: true }
+                {name: "可爱多", num: "9,567", flag: true},
+                {name: "娃哈哈", num: "2,345", flag: false},
+                {name: "喜之郎", num: "9,037", flag: false},
+                {name: "八喜", num: "1,080", flag: true},
+                {name: "小洋人", num: "4,724", flag: false},
+                {name: "好多鱼", num: "9,999", flag: true}
             ]
         }
     ];
-    //  2. 根据数据渲染各省热销 sup 模块内容
-    // (1) 遍历 hotData对象
+    //  根据数据渲染各省热销 sup 模块内容
     var supHTML = "";
-    $.each(hotData, function(index, item) {
-        // console.log(item);
-        supHTML += `<li><span>${item.city}</span><span> ${item.sales} <s class=
-    ${item.flag ? "icon-up" : "icon-down"}></s></span></li>`;
+    var index = 0;
+    $.each(hotData, function (index, item) {
+        supHTML += `<li><span>${item.city}</span><span> ${item.sales}<s class= ${item.flag ? "icon-up" : "icon-down"}></s></span></li>`;
     });
-    // 把生成的5个小li字符串给 sub dom盒子
     $(".sup").html(supHTML);
-    // 3. 当鼠标进入 tab 的时候
+
     // 鼠标经过当前的小li要高亮显示
-    $(".province .sup").on("mouseenter", "li", function() {
+    $(".province .sup").on("mouseenter", "li", function () {
         index = $(this).index();
         render($(this));
     });
 
     // 声明一个函数 里面设置sup当前小li高亮还有 对应的品牌对象渲染
-    // 这个函数需要传递当前元素进去
     function render(currentEle) {
         currentEle
             .addClass("active")
             .siblings()
             .removeClass();
-        // 拿到当前城市的品牌对象
-        // console.log($(this).index());
-        // 可以通过hotData[$(this).index()] 得到当前的城市
-        // console.log(hotData[$(this).index()]);
-        // 我们可以通过hotData[$(this).index()].brands 拿到的是城市对象的品牌种类
-        // console.log(hotData[$(this).index()].brands);
-        // 开始遍历品牌数组
         var subHTML = "";
-        $.each(hotData[currentEle.index()].brands, function(index, item) {
-            // 是对应城市的每一个品牌对象
-            // console.log(item);
-            subHTML += `<li><span>${item.name}</span><span> ${item.num}<s class=
-    ${item.flag ? "icon-up" : "icon-down"}
-    ></s></span></li>`;
+        $.each(hotData[currentEle.index()].brands, function (index, item) {
+            subHTML += `<li><span>${item.name}</span><span> ${item.num}<s class=${item.flag ? "icon-up" : "icon-down"}></s></span></li>`;
         });
         // 把生成的6个小li字符串给 sub dom盒子
         $(".sub").html(subHTML);
     }
-    // 4. 默认把第一个小li处于鼠标经过状态
+
+
+    //默认把第一个小li处于鼠标经过状态
     var lis = $(".province .sup li");
     lis.eq(0).mouseenter();
-    // 5 开启定时器
-    var index = 0;
-    var timer = setInterval(function() {
+
+
+    //  开启定时器
+    var timer = setInterval(function () {
         index++;
         if (index >= 5) index = 0;
-        // lis.eq(index).mouseenter();
         render(lis.eq(index));
     }, 2000);
 
     $(".province .sup").hover(
+        //！！！$( selector ).hover( handlerIn, handlerOut )===$( selector ).mouseenter( handlerIn ).mouseleave( handlerOut );
         // 鼠标经过事件
-        function() {
+        function () {
             clearInterval(timer);
         },
         // 鼠标离开事件
-        function() {
+        function () {
             clearInterval(timer);
-            timer = setInterval(function() {
+            timer = setInterval(function () {
                 index++;
                 if (index >= 5) index = 0;
-                // lis.eq(index).mouseenter();
                 render(lis.eq(index));
             }, 2000);
         }
